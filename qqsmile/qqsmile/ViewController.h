b5b64fa7e07d386ccf1e071ea5fe65d481289c27
QiZhang <qizhang@tvunetworks.com>
2016-07-12 17:45:46 +0800

    Init commit

---
+++ qqsmile/qqsmile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qqsmile
+//
+//  Created by zhangqi on 12/7/2016.
+//  Copyright (c) 2016 zhangqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ qqsmile/qqsmile/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  qqsmile
+//
+//  Created by zhangqi on 12/7/2016.
+//  Copyright (c) 2016 zhangqi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
