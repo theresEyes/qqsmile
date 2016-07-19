3208e38f0de98f33e0d84d887dbcc58a9caef44b
QiZhang <qizhang@tvunetworks.com>
2016-07-19 15:01:34 +0800

    Add video preview layer

--- qqsmile/qqsmile/ViewController.m
+++ qqsmile/qqsmile/ViewController.m
@@ -7,16 +7,81 @@
 //
 
 #import "ViewController.h"
-
+#import <AVFoundation/AVFoundation.h>
 @interface ViewController ()
+@property (nonatomic,strong) AVCaptureSession *avCaptureSession;
+@property (nonatomic,strong) AVCaptureDevice *captureDevice;
+@property (nonatomic,strong) AVCaptureDeviceInput *captureDeviceInput;
+@property (nonatomic,strong) AVCaptureMovieFileOutput *captureMovieFileOutput;
+@property (nonatomic,strong) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer;
 
 @end
 
 @implementation ViewController
 
+- (AVCaptureSession *)avCaptureSession
+{
+    if (!_avCaptureSession) {
+        _avCaptureSession = [[AVCaptureSession alloc] init];
+        _captureDevice = [self getCameraDeviceWithPosition:AVCaptureDevicePositionBack];
+        NSError *error = nil;
+        _captureDeviceInput = [AVCaptureDeviceInput deviceInputWithDevice:_captureDevice error:&error];
+        _captureMovieFileOutput = [[AVCaptureMovieFileOutput alloc] init];
+        
+        if ([_avCaptureSession canSetSessionPreset:AVCaptureSessionPreset1280x720]) {
+            _avCaptureSession.sessionPreset = AVCaptureSessionPreset1280x720;
+        }
+        
+        if ([_avCaptureSession canAddInput:_captureDeviceInput]) {
+            [_avCaptureSession addInput:_captureDeviceInput];
+        }
+        
+        AVCaptureConnection *captureConnection=[_captureMovieFileOutput connectionWithMediaType:AVMediaTypeVideo];
+        if ([captureConnection isVideoStabilizationSupported ]) {
+            captureConnection.preferredVideoStabilizationMode=AVCaptureVideoStabilizationModeAuto;
+        }
+        
+        if ([_avCaptureSession canAddOutput:_captureMovieFileOutput]) {
+            [_avCaptureSession addOutput:_captureMovieFileOutput];
+        }
+    }
+    return _avCaptureSession;
+}
+
+- (AVCaptureVideoPreviewLayer *)captureVideoPreviewLayer
+{
+    if (!_captureMovieFileOutput) {
+        _captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.avCaptureSession];
+    }
+    return _captureVideoPreviewLayer;
+}
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    CALayer *layer = self.view.layer;
+    layer.masksToBounds = YES;
+    self.captureVideoPreviewLayer.frame = layer.bounds;
+    self.captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResize;
+    [layer insertSublayer:self.captureVideoPreviewLayer below:self.view.layer];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.avCaptureSession startRunning];
+}
+
+-(AVCaptureDevice *)getCameraDeviceWithPosition:(AVCaptureDevicePosition )position{
+    NSArray *cameras= [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    for (AVCaptureDevice *camera in cameras) {
+        if ([camera position] == position) {
+            return camera;
+            
+        }
+    }
+    return nil;
 }
 
 - (void)didReceiveMemoryWarning {
